e1b1b3a7910163e39e07453b2ad508ef59589954
Xavier Puspus <xpuspus@gmail.com>
2020-06-16 14:54:50 +0800

    Added long description for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
   name = 'parity-fairness',         # How you named your package folder (MyLib)
   packages = ['parity-fairness'],   # Chose the same as "name"
-  version = 'v_01.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Measure bias from data and machine learning models.',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type='text/markdown'
   author = 'Xavier M. Puspus',                   # Type in your name
   author_email = 'xpuspus@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/xmpuspus/parity-fairness',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/xmpuspus/parity-fairness/archive/v_01.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/xmpuspus/parity-fairness/archive/v_01.2.tar.gz',    # I explain this later on
   keywords = ['fairness', 'bias', 'explainability', 'AI', 'machine learning', 'data'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'matplotlib',
