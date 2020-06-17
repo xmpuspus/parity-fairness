0d0bc29ff85e2416233129950c0f56abd7de89c5
Xavier Puspus <xpuspus@gmail.com>
2020-06-17 09:49:10 +0800

    Added pypi doc

--- setup.py
+++ setup.py
@@ -1,22 +1,31 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
 
 # from os import path
 # this_directory = path.abspath(path.dirname(__file__))
 # with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 #     long_description = f.read()
 
+# The directory containing this script
+HERE = pathlib.Path(__file__).parent
 
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# Call setup
 setup(
-  name = 'parity-fairness',         # How you named your package folder (MyLib)
-  packages = ['parity'],   # Chose the same as "name"
-  version = '0.1.8',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Measure bias from data and machine learning models.',   # Give a short description about your library
-  author = 'Xavier M. Puspus',                   # Type in your name
-  author_email = 'xpuspus@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/xmpuspus/parity-fairness',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/xmpuspus/parity-fairness/archive/v_01.8.tar.gz',    # I explain this later on
-  keywords = ['fairness', 'bias', 'explainability', 'AI', 'machine learning', 'data'],   # Keywords that define your package best
+  name = 'parity-fairness', # How you named your package folder (MyLib)
+  packages = ['parity'], # Chose the same as "name"
+  version = '0.1.11', # Start with a small number and increase it with every change you make
+  license='MIT', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Measure bias from data and machine learning models.', # Give a short description about your library
+  long_description=README,
+  long_description_content_type="text/markdown",
+  author = 'Xavier M. Puspus', # Type in your name
+  author_email = 'xpuspus@gmail.com', # Type in your E-Mail
+  url = 'https://github.com/xmpuspus/parity-fairness', # Provide either the link to your github or to your website
+  download_url = 'https://github.com/xmpuspus/parity-fairness/archive/v_01.11.tar.gz', # I explain this later on
+  keywords = ['fairness', 'bias', 'explainability', 'AI', 'machine learning', 'data'], # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'matplotlib',
           'joblib',
