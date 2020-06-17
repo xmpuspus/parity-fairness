28b80837a534e8025cdaa849c2ce9c9d07683b99
Xavier Puspus <xpuspus@gmail.com>
2020-06-17 13:57:35 +0800

    Added more importable modules

--- parity/__init__.py
+++ parity/__init__.py
@@ -2,3 +2,8 @@ from parity.fairness_metrics import show_bias, prepare_data, get_fair_metrics_an
 from parity.fairness_metrics import add_to_df_algo_metrics, plot_model_performance, get_model_performance
 from parity.fair import fairness_dashboard, decode_dataset, get_attributes, convert_to_pd_dataframe, disparate_impact_remover, learning_fair_representation
 from parity.fair import reweight, structured_data_train_test_split, adversarial_debias, prejudice_remover, calibrate_equality_of_odds, reject_option
+from parity.explainer import feature_importances, shap_feature_explainer, plot_prediction_causes, dependence_plots
+from parity.counterfatual_explainer import get_data_object, get_explainer_object, generate_counterfactual
+from parity.deep_explainer import shap_values, mimic_values, dashboard
+from parity.reduction import gridSearch, show_comparison
+from parity.thresholdOptimizer import thresholdOptimizer, show_comparison
