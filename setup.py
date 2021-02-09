6d26266a11bdb8e72840577bacd639fcbce2329b
nam <nam@zerosixer.com>
2021-02-09 11:19:16 +0800

    updated zar file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ README = (HERE / "README.md").read_text()
 setup(
   name = 'parity-fairness', # How you named your package folder (MyLib)
   packages = ['parity'], # Chose the same as "name"
-  version = '0.1.29', # Start with a small number and increase it with every change you make
+  version = '0.1.30', # Start with a small number and increase it with every change you make
   license='MIT', # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Measure bias from data and machine learning models.', # Give a short description about your library
   long_description=README,
@@ -24,7 +24,7 @@ setup(
   author = 'Xavier M. Puspus', # Type in your name
   author_email = 'xpuspus@gmail.com', # Type in your E-Mail
   url = 'https://github.com/xmpuspus/parity-fairness', # Provide either the link to your github or to your website
-  download_url = 'https://github.com/xmpuspus/parity-fairness/archive/v_01.29.tar.gz', # I explain this later on
+  download_url = 'https://github.com/xmpuspus/parity-fairness/archive/v_01.30.tar.gz', # I explain this later on
   keywords = ['fairness', 'bias', 'explainability', 'AI', 'machine learning', 'data'], # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'matplotlib',
@@ -46,7 +46,7 @@ setup(
           'boto3'],
     
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
@@ -55,6 +55,8 @@ setup(
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
   ],
 )
  
